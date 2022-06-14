e3289809d3f6bcd43c94e0964699efefb523decf
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-13 21:49:08 -0400

    add test folder/file for nav

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe('Nav Component', () => {
+    // baseline test
+
+    //  snapshot test
+})
