20662683d8da8c2f7108aa735cda1607bb8550b1
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-13 20:55:54 -0400

    update formatting

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 
 function App() {
     const categories = [
-        {
-            name: "commercial",
-            description: "Photos of grocery stores, food trucks, and other commercial projects",
+        { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects",
         },
         { name: "portraits", description: "Portraits of people in my life" },
         { name: "food", description: "Delicious delicacies" },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
     ];
     function categorySelected(name) {
         console.log(`${name} clicked`)
