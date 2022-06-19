b4fdcc76892a57633d6d54b407eded9ff56e7f75
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-19 11:45:06 -0400

    display photos with hooks

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
