f6135f49ac05f7d8ba587eec9442df5312f9a1cc
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-20 18:22:07 -0400

    add modal component:

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Modal() {
+    return (
+      // JSX
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">Photo Name </h3>
+                <img 
+                    src={require(`../../assets/small/${category}/${i}.jpg`)}
+                    alt="current category" 
+                    className='img-thumbnail mx-1'
+                    onClick={() => toggleModal(image, i)}
+                    key={image.name}
+                    />
+                <p>Photo Description</p>
+                <button type="button">Close this modal</button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
     const [photos] = useState([
@@ -103,6 +104,7 @@ const PhotoList = ({ category }) => {
 
     return (
         <div>
+            <Modal />
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
                     <img 
