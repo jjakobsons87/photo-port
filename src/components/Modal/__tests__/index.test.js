4419bfdfbe5733a3dfe02574490787fe04a42359
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-20 22:26:03 -0400

    update tests

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import PhotoList from "../";
+import Modal from "../";
 
+const mockToggleModal = jest.fn();
 const currentPhoto = {
     name: 'Park bench',
     category: 'landscape',
@@ -10,15 +11,23 @@ const currentPhoto = {
     index: 1
 };
 
-const mockToggleModal = jest.fn();
-
 afterEach(cleanup);
 
 describe('Modal component', () => {
     it('renders', () => {
-        // vaseline render omponent test 
+       render(<Modal
+        onClose={mockToggleModal}
+        currentPhoto={currentPhoto}
+        />);
     })
-    // snapshot test 
+    
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<Modal
+            onClose={mockToggleModal}
+            currentPhoto={currentPhoto}
+        />);
+        expect(asFragment()).toMatchSnapshot();
+    })
 })
 
 describe('Click Event', () => {
